e5029a7c125956180ec645da1a7232748d3465c0
OlichkaKhamuliak <olkoboyko97@gmail.com>
2024-02-18 13:30:22 +0200

    task3

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,13 +1,17 @@
-import { Container, CountryInfo, Loader, Section } from 'components';
+import { Container, CountryInfo, GoBackBtn, Loader, Section } from 'components';
 import { useFetchCountry } from '../hooks/useFetchCountry';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useRef } from 'react';
 
 const Country = () => {
   const { country, loading, error } = useFetchCountry();
-  console.log('🚀 ~ country:', country);
+  const location = useLocation();
+  const goBack = useRef(location?.state?.from ?? '/');
+  console.log(location);
   return (
     <Section>
       <Container>
+        <GoBackBtn path={goBack.current}>Back to countries</GoBackBtn>
         {loading && <Loader />}
         {error && <Navigate to="/" replace />}
         {country && <CountryInfo country={country} />}
